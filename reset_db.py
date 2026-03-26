ad44066e2b8d5172aac315027fe33ea8b102c70a
ivan li <ivanli9355@gmail.com>
2026-03-25 22:43:49 -0700

    Remove reset_db.py from tracking and apply .gitignore

--- reset_db.py
+++
@@ -1,7 +0,0 @@
-from src.app.database.models import Base
-from src.app.database.db import engine
-
-def reset_db():
-    print("WARNING: Deleting ALL application data...")
-    Base.metadata.drop_all(bind=engine)
-    print("Database is now empty.")
